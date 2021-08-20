81732b0b227310a4009dae0a53e7848c77d0205d
Wendy-Omondi <wendyomi4@gmail.com>
2021-08-20 00:33:07 +0000

    log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Regex-ing, Log formatter, Create logger, Connect to secure database,
     Read and filter data, Encrypting passwords, Check valid password
 """
+import logging
 from typing import List
 import re
 
@@ -14,3 +15,22 @@ def filter_datum(fields: List[str], redaction: str,
                          i + "=" + redaction + separator,
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filters values in incoming log records using filter_datum"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
